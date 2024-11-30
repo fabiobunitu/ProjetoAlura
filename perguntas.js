0a4e992881b20a81bdc5a5351c7b03f176de6c23
fabiobunitu <fabioaugustocarlim@gmail.com>
2024-11-30 19:51:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vôlei',
+    'Qual posição do vôlei faz levantamentos?',
+    'É o levantador. É aquele jogador responsável por armar os ataques. Ou seja, ele “levanta” ou passa a bola para o jogador que irá atacar na sequência.'
+)
+
+criaCartao(
+    'Vôlei',
+    'Qual a história do vôlei?',
+    'O voleibol surgiu nos Estados Unidos em 1895. Seu criador foi William George Morgan (1870-1942). Na época, Morgan era chefe de Educação Física da “Associação Cristã de Moços” (ACM) em Massachusetts.'
+)
+
+criaCartao(
+    'Vôlei',
+    'Existem quantas posições no vôlei?',
+    'As posições no vôlei incluem levantador, oposto, ponteiro, central e líbero. O levantador é quem arma as jogadas, o oposto é o principal atacante, os ponteiros atacam e defendem, os centrais bloqueiam, e os líberos se dedicam à defesa e recepção.'
+)
+
+criaCartao(
+    'Vôlei',
+    'Quais as medidas da quadra de vôlei?',
+    'A quadra oficial de vôlei tem o formato de um retângulo e suas medidas são 18 por 9 metros. Geralmente, as quadras de voleibol possuem uma cor alaranjada, contornadas por uma zona menor de cor verde.'
+)
